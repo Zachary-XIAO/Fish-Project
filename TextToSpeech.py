ae9f9dcdf34f2f7fd4c2d83aa7891304904fcb21
XIAO Zhe <90884677+Zheanl@users.noreply.github.com>
2022-12-02 19:19:59 +0800

    Update TextToSpeech.py

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -1,10 +1,40 @@
 from google.cloud import texttospeech
+from playsound import playsound
 import os
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'polyu-wkwf-99b942e126ff.json'
+# For medicine remind, use take_medicine_notice_audio()
 # For hungry, use hungry_audio()
 # For tired, use tiring_audio()
 # For dialog starting, use dialog_starting_audio()
+# For dialog closing, use dialog_closing_audio()
+
+
+def take_medicine_notice_audio():
+    client = texttospeech.TextToSpeechClient()
+
+    synthesis_input = texttospeech.SynthesisInput(
+        text="你好，现在该吃药了！")
+
+    voice = texttospeech.VoiceSelectionParams(
+        # language_code='en-gb',
+        # name='en-GB-Standard-A',
+        language_code='zh-CN',
+        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE)
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3, speaking_rate=1)
+    #  valid speaking_rate is between 0.25 and 4.0
+    response = client.synthesize_speech(
+        input=synthesis_input,
+        voice=voice,
+        audio_config=audio_config)
+    with open('SpeechCache.wav', 'wb') as out:
+        out.write(response.audio_content)
+        print('Hungry audio content written to file "SpeechCache.wav"')
+    file = 'SpeechCache.wav'
+    os.system(file)
+
 
 def hungry_audio():
     client = texttospeech.TextToSpeechClient()
@@ -25,11 +55,12 @@ def hungry_audio():
         input=synthesis_input,
         voice=voice,
         audio_config=audio_config)
-    with open('SpeechCache.wav', 'wb') as out:
+    with open('./SpeechCache.wav', 'wb') as out:
         out.write(response.audio_content)
         print('Hungry audio content written to file "SpeechCache.wav"')
-    file = 'SpeechCache.wav'
+    file = './SpeechCache.wav'
     os.system(file)
+
 
 def tiring_audio():
     client = texttospeech.TextToSpeechClient()
@@ -56,11 +87,12 @@ def tiring_audio():
     file = 'SpeechCache.wav'
     os.system(file)
 
+
 def dialog_starting_audio():
     client = texttospeech.TextToSpeechClient()
 
     synthesis_input = texttospeech.SynthesisInput(
-        text="语音对话启动！一起聊天吧！")
+        text="语音对话已经启动！一起聊天吧！")
 
     voice = texttospeech.VoiceSelectionParams(
         # language_code='en-gb',
@@ -80,6 +112,7 @@ def dialog_starting_audio():
         print('dialog starting audio content written to file "SpeechCache.wav"')
     file = 'SpeechCache.wav'
     os.system(file)
+
 
 def dialog_closing_audio():
     client = texttospeech.TextToSpeechClient()
